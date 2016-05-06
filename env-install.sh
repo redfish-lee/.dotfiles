bcecc7a41a957e8b1b60f06afc551608de41332c
Leo Lee <redfish-lee@gmail.com>
2016-05-06 20:52:45 +0800

    Initialize install.sh commit

---
+++ env-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Installation:
+#   This shell will install basic necessary environment tools.
+# History:
+#   2016/05/06  Redfish Hung-Yu Li  First release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+echo -e "Hello World! \a \n"
+exit 0
